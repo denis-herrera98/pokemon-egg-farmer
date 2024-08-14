3abaf13c148f8bf85eb1af2bd789ed703635c2ba
Denis Herrera Torres <denis.herrera.torres98@gmail.com>
2024-08-14 09:18:21 -0600

    Ditto farming

--- main.c
+++ main.c
@@ -34,6 +34,7 @@ bool wasItemFound = false;
 bool firstTimeAttacking = true;
 bool gotItem = false;
 bool isReadyToFight = false;
+Window window = 0x1c00007; 
  
 void restartState(){ 
     blockCompleted = false;
@@ -247,18 +248,31 @@ enum Actions getActionToExecuteFishing(char *text){
     return 0;
 }
 
-//void simulate_keypress(window, Display *display, KeySym key) {
-//    KeyCode keycode = XKeysymToKeycode(display, key);
-//    
-//    // Press the key
-//    XTestFakeKeyEvent(display, keycode, True, 0);
-//    XFlush(display);
-//
-//    // Release the key
-//    XTestFakeKeyEvent(display, keycode, False, 0);
-//    XFlush(display);
-//    sleep(1);
-//}
+void simulateKeyPressFocusWin(Display *display, KeySym key) {
+    KeyCode keycode = XKeysymToKeycode(display, key);
+    
+    // Press the key
+    XTestFakeKeyEvent(display, keycode, True, 0);
+    XFlush(display);
+
+    usleep(80000);
+    // Release the key
+    XTestFakeKeyEvent(display, keycode, False, 0);
+    XFlush(display);
+}
+
+void simulateKeyPressFocusWinDelay(int delay, Display *display, KeySym key) {
+    KeyCode keycode = XKeysymToKeycode(display, key);
+    
+    // Press the key
+    XTestFakeKeyEvent(display, keycode, True, 0);
+    XFlush(display);
+
+    usleep(delay);
+    // Release the key
+    XTestFakeKeyEvent(display, keycode, False, 0);
+    XFlush(display);
+}
 
 Window get_focused_window(Display *display) {
     Window focused_window;
@@ -270,45 +284,42 @@ Window get_focused_window(Display *display) {
     return focused_window;
 }
 
-void simulate_keypress(Window window, Display *display, KeySym key) {
-    Window activeWindow = get_focused_window(display);
-
-//    printf("0x%lx\n", activeWindow);
-//    printf("0x%lx\n", window);
-    XKeyEvent event;
-    event.display = display;
-    event.window = window;
-    event.root = DefaultRootWindow(display);
-    event.subwindow = None;
-    event.time = CurrentTime;
-    event.x = 0;
-    event.y = 0;
-    event.x_root = 0;
-    event.y_root = 0;
-    event.same_screen = True;
-    event.state = 0;
-    event.keycode = XKeysymToKeycode(display, key);
-    event.type = KeyPress;
-
-    XSetInputFocus(display, window, RevertToNone, CurrentTime);
-    XSendEvent(display, window, True, KeyPressMask, (XEvent *)&event);
-    XFlush(display);
-    usleep(40000);
-    event.type = KeyRelease;
-    XSendEvent(display, window, True, KeyReleaseMask, (XEvent *)&event);
-    XSetInputFocus(display, activeWindow, RevertToNone, CurrentTime);
-    XFlush(display);
-}
+//void simulateKeyPressFocusWin(Display *display, KeySym key) {
+//    // Window activeWindow = get_focused_window(display);
+//    XKeyEvent event;
+//    event.display = display;
+//    event.window = window;
+//    event.root = DefaultRootWindow(display);
+//    event.subwindow = None;
+//    event.time = CurrentTime;
+//    event.x = 0;
+//    event.y = 0;
+//    event.x_root = 0;
+//    event.y_root = 0;
+//    event.same_screen = True;
+//    event.state = 0;
+//    event.keycode = XKeysymToKeycode(display, key);
+//    event.type = KeyPress;
+//
+//    // XSetInputFocus(display, window, RevertToNone, CurrentTime);
+//    XSendEvent(display, window, True, KeyPressMask, (XEvent *)&event);
+//    XFlush(display);
+//    usleep(40000);
+//    event.type = KeyRelease;
+//    XSendEvent(display, window, True, KeyReleaseMask, (XEvent *)&event);
+//    // XSetInputFocus(display, activeWindow, RevertToNone, CurrentTime);
+//    XFlush(display);
+//}
 
 
-void executeAction(Window window, Display *display, enum Actions action) {
+void executeAction(Display *display, enum Actions action) {
     switch (action) {
         case BLOCK:
             printf("Starting block action.\n");
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_K); 
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_K); 
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
             printf("Block action done.\n");
             blockCompleted = true;
             break;
@@ -318,56 +329,56 @@ void executeAction(Window window, Display *display, enum Actions action) {
             break;
         case ESCAPE:
             printf("Starting escape action.\n");
-            simulate_keypress(window, display, XK_J); 
-            simulate_keypress(window, display, XK_K); 
-            simulate_keypress(window, display, XK_Z); 
+            simulateKeyPressFocusWin(display, XK_J); 
+            simulateKeyPressFocusWin(display, XK_K); 
+            simulateKeyPressFocusWin(display, XK_Z); 
             printf("Escape action done.\n");
             restartState();
             break;
         case START_COMBAT:
             printf("Starting combat action.\n");
-            simulate_keypress(window, display, XK_1);
+            simulateKeyPressFocusWin(display, XK_3);
             printf("Start combat action done.\n");
             isInCombat = true;
             break;
         case START_FISHING:
             printf("Starting combat action.\n");
-            simulate_keypress(window, display, XK_2);
+            simulateKeyPressFocusWin(display, XK_2);
             break;
         case ATTACK_LUV:
             printf("Starting attack action.\n");
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
             printf("attack action done.\n");
             break;
         case CHANGE_POKEMON:
             printf("Starting combat action.\n");
-            simulate_keypress(window, display, XK_J); 
-            simulate_keypress(window, display, XK_K); 
-            simulate_keypress(window, display, XK_Z); 
+            simulateKeyPressFocusWin(display, XK_J); 
+            simulateKeyPressFocusWin(display, XK_K); 
+            simulateKeyPressFocusWin(display, XK_Z); 
             printf("Start combat action done.\n");
             isInCombat = true;
             break;
         case ATTACK:
             printf("Starting attack action.\n");
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
             if (firstTimeAttacking){
-                simulate_keypress(window, display, XK_J);
+                simulateKeyPressFocusWin(display, XK_H);
                 firstTimeAttacking = false;
             }
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
             printf("attack action done.\n");
             break;
         case REMOVE_OBJECT:
             printf("\nStarting removing action.\n");
-            simulate_keypress(window, display, XK_D);
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_Z);
-            simulate_keypress(window, display, XK_J); 
-            simulate_keypress(window, display, XK_J); 
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_D);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_J); 
+            simulateKeyPressFocusWin(display, XK_J); 
+            simulateKeyPressFocusWin(display, XK_Z);
             gotItem = false;
             printf("\nremoving action done.\n");
         default:
@@ -376,27 +387,55 @@ void executeAction(Window window, Display *display, enum Actions action) {
     }
 }
 
-int main() {
+
+// exp
+int main4() {
+
+    Display *display = XOpenDisplay(NULL);
+
+    if (display == NULL) {
+        fprintf(stderr, "Unable to open X display\n");
+        return 1;
+    }
+
+    sleep(2);
+
+    while (1) {
+
+        simulateKeyPressFocusWin(display, XK_3);
+        sleep(5);
+        simulateKeyPressFocusWin(display, XK_Z);
+        simulateKeyPressFocusWin(display, XK_Z);
+        simulateKeyPressFocusWin(display, XK_Z);
+        sleep(3);
+    }
+
+    XCloseDisplay(display);
+
+    return 0;
+    
+}
+
+// FISHING
+int main2() {
 
     // Run i3-msg command
-//    fp = popen("i3-msg -s /run/user/1000/i3/ipc-socket.53867 -t get_tree", "r");
+//    fp = popen("i3-msg -s /run/user/1000/i3/ipc-socket.2185 -t get_tree", "r");
 //    if (fp == NULL) {
 //        perror("popen");
 //        return 1;
 //    }
 
-    Display *display = XOpenDisplay(NULL);
-
 //xdotool windowfocus 56623111 key XK_J 
-
     // Window window = find_window_by_name(display, DefaultRootWindow(display), windowName);
     
-    Window window = 0x3400007; 
+    Display *display = XOpenDisplay(NULL);
+    sleep(2);
 
     while (1) {
 
         printf("Starting fishing action.\n");
-        simulate_keypress(window, display, XK_2);
+        simulateKeyPressFocusWin(display, XK_2);
         sleep(4);
         printf("Fishing completed, waiting for fishing dialog.\n");
 
@@ -408,11 +447,11 @@ int main() {
 
         if (strstr(fishingDialogText, "picado") == NULL) {
             printf("\nNothing captured\n");
-            simulate_keypress(window, display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
             continue;
         }
 
-        simulate_keypress(window, display, XK_Z);
+        simulateKeyPressFocusWin(display, XK_Z);
         printf("Waiting dialogs\n");
         sleep(13);
         printf("Waiting dialogs END\n");
@@ -428,18 +467,18 @@ int main() {
             break;
         }
 
-        char* luvdisc = "frisked Luvdisc";
+        char* luvdisc = "frisked";
         if (strstr(text, luvdisc) == NULL) {
             printf("No item was found\n");
-            executeAction(window, display, ESCAPE);
+            executeAction(display, ESCAPE);
             sleep(4);
             continue;
         }
         printf("ITEM FOUND!!\n");
 
-        executeAction(window, display, ATTACK_LUV);
+        executeAction(display, ATTACK_LUV);
         sleep(11);
-        executeAction(window, display, REMOVE_OBJECT);
+        executeAction(display, REMOVE_OBJECT);
         free(text);
     }
 
@@ -448,18 +487,10 @@ int main() {
     return 0;
 }
 
+// HUEVOS
 int main1() {
 
     Display *display = XOpenDisplay(NULL);
-    Window window = 52428807; // Window ID from your JSON data
-//    char* windowName = "РokеМMO";
-//    Window window = find_window_by_name(display, DefaultRootWindow(display), "РokеМMO");
-
- //   if (window == 0) {
- //       fprintf(stderr, "Unable to find %s\n window", windowName);
- //       return 1;
- //   }
-
 
     if (display == NULL) {
         fprintf(stderr, "Unable to open X display\n");
@@ -469,7 +500,9 @@ int main1() {
     sleep(3);
     while (1) {
 
-        int fightScreenShot = captureScreen(display, 300, 640, WIDTH, HEIGHT - 20, "readyToPlay.ppm");
+        simulateKeyPressFocusWin(display, XK_3);
+
+        int fightScreenShot = captureScreen(display, 1920 + 300, 640, WIDTH, HEIGHT - 20, "readyToPlay.ppm");
         if (fightScreenShot != 0) {
             return 1;
             break;
@@ -485,12 +518,15 @@ int main1() {
 
         if (isReadyToFight || !isInCombat || gotItem) {
 
-            int screenshotSuccess = captureScreen(display, POSX, 830, WIDTH, HEIGHT, "chatScreenshot.ppm");
+            int screenshotSuccess = captureScreen(display, 1920 + POSX, 830, WIDTH, HEIGHT, "chatScreenshot.ppm");
+            sleep(1);
+
             if (screenshotSuccess != 0) {
                 return 1;
                 break;
             }
 
+            sleep(1);
             char* text = readImageText("chatScreenshot.ppm");
             if (text == NULL) {
                 return 1;
@@ -498,13 +534,8 @@ int main1() {
             }
 
             printf("LOOKING FOR ACTIONS TO EXECUTE");
-            enum Actions action = 0;
-            if (0) {
-                action = getActionToExecute(text);
-            } else {
-                action = getActionToExecuteFishing(text);
-            }
-            executeAction(window, display, action);
+            enum Actions action = getActionToExecute(text);
+            executeAction(display, action);
             free(text);
             free(fightText);
 
@@ -513,6 +544,146 @@ int main1() {
         }   
 
         sleep(3);
+    }
+
+    XCloseDisplay(display);
+
+    return 0;
+}
+
+int main() {
+
+    Display *display = XOpenDisplay(NULL);
+
+    if (display == NULL) {
+        fprintf(stderr, "Unable to open X display\n");
+        return 1;
+    }
+
+    sleep(3);
+    while (1) {
+
+
+        int fightScreenShot = captureScreen(display, 1920 + 300, 640, WIDTH, HEIGHT - 20, "readyToPlay.ppm");
+        if (fightScreenShot != 0) {
+            return 1;
+            break;
+        }
+
+        char* fightText = readImageText("readyToPlay.ppm");
+        if (fightText == NULL) {
+            return 1;
+            break;
+        }
+
+        sleep(2);
+        isReadyToFight = strstr(fightText, "Troply") != NULL;
+
+        if (isReadyToFight || strstr(fightText, "FIGHT") != NULL) {
+
+            int screenshotSuccess = captureScreen(display, 1920 + POSX, 906, WIDTH, 130, "chatScreenshot.ppm");
+
+            if (screenshotSuccess != 0) {
+                return 1;
+                break;
+            }
+
+            sleep(2);
+            char* text = readImageText("chatScreenshot.ppm");
+            if (text == NULL) {
+                return 1;
+                break;
+            }
+            if (strstr(text, "UnDitto") == NULL) {
+                executeAction(display, ESCAPE);
+                continue;
+            }
+            
+            sleep(4);
+            printf("DOING VOLTIOCAMBIO\n");
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            sleep(12);
+            printf("DOING VOLTIOCAMBIO DONE\n");
+
+            printf("SWITCHING TO BRELOOM\n");
+            simulateKeyPressFocusWin(display, XK_K);
+            simulateKeyPressFocusWin(display, XK_Z);
+            sleep(8);
+            printf("SWITCHING TO BRELOOM DONE\n");
+
+
+            // BREELOM
+            printf("ESPORA\n");
+            simulateKeyPressFocusWin(display, XK_H);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            sleep(14);
+            printf("ESPORA DONE\n");
+
+
+            printf("THORWING POKEBALL\n");
+            simulateKeyPressFocusWin(display, XK_K);
+            simulateKeyPressFocusWin(display, XK_Z);
+            simulateKeyPressFocusWin(display, XK_Z);
+            sleep(14);
+            printf("THORWING POKEBALL DONE\n");
+
+
+            int capturedScreenshot = captureScreen(display, 1920 + POSX, 906, WIDTH, 130, "capturedScreenshot.ppm");
+            sleep(1);
+
+            if (screenshotSuccess != 0) {
+                return 1;
+                break;
+            }
+
+            sleep(1);
+            char* capturedText = readImageText("capturedScreenshot.ppm");
+            if (capturedText == NULL) {
+                return 1;
+                break;
+            }
+
+            while (strstr(capturedText, "Ditto atrapado!") == NULL) {
+
+                printf("POKEBALL FAILED\n");
+                printf("TRYING AGAIN POKEBALL\n");
+                simulateKeyPressFocusWin(display, XK_K);
+                simulateKeyPressFocusWin(display, XK_Z);
+                simulateKeyPressFocusWin(display, XK_Z);
+                printf("POKEBALL WAS THREW\n");
+
+                capturedScreenshot = captureScreen(display, 1920 + POSX, 906, WIDTH, 130, "capturedScreenshot.ppm");
+                sleep(1);
+
+                if (screenshotSuccess != 0) {
+                    return 1;
+                    break;
+                }
+
+                sleep(1);
+                capturedText = readImageText("capturedScreenshot.ppm");
+                if (capturedText == NULL) {
+                    return 1;
+                    break;
+                }
+                sleep(5);
+
+            }
+            
+
+            free(text);
+            free(capturedText);
+            free(fightText);
+
+        } else {
+          printf("SKIPPING THIS TURN\n");
+          simulateKeyPressFocusWinDelay(1000000, display, XK_H);
+          simulateKeyPressFocusWinDelay(1000000, display, XK_K);
+        }   
+
+        sleep(2);
     }
 
     XCloseDisplay(display);
